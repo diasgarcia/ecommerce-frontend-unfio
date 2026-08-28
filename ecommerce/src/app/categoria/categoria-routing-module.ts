e9c9edbe7aff6748b2971ae240d7ee07ae35a214
Rafael <138343340+diasgarcia@users.noreply.github.com>
2026-08-28 01:08:26 +0000

    criando rotas para categoria. cadastro e categoria.listagem

--- ecommerce/src/app/categoria/categoria-routing-module.ts
+++ ecommerce/src/app/categoria/categoria-routing-module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Cadastro } from './cadastro/cadastro';
+import { Listagem } from './listagem/listagem';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'cadastro', 
+    component: Cadastro,
+  },
+  {
+    path: 'listagem', 
+    component: Listagem,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

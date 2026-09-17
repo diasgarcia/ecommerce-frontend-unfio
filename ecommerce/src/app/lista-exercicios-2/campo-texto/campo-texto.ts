3c1f7bc4c7c2614e85490e423037a0c96bcadc28
Rafael <138343340+diasgarcia@users.noreply.github.com>
2026-09-17 23:46:56 +0000

    exmplo proxima aula

---
+++ ecommerce/src/app/lista-exercicios-2/campo-texto/campo-texto.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-campo-texto',
+  standalone: false,
+  templateUrl: './campo-texto.html',
+  styleUrl: './campo-texto.scss',
+})
+export class CampoTexto {}
